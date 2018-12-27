bb583b1e24df346795fe8cedcc05047cc12b644a
Alysson Lopes <alyssondlopes@gmail.com>
2018-12-27 12:24:32 -0300

    fix: Correções do pylint

--- subtitle.py
+++ subtitle.py
@@ -2,25 +2,32 @@ import cv2
 from datetime import timedelta
 from pytesseract import image_to_string
 from math import ceil, floor
+
+
 def getTimeStamp(sec):
     timestamp = str(timedelta(seconds=sec))
     start = '{},000'.format(timestamp)
     end = '{},999'.format(timestamp)
     return '{} --> {}'.format(start, end)
 
-def getSubtitleGroup(seq, timestamp,text):
+
+def getSubtitleGroup(seq, timestamp, text):
     return '{}\n{}\n{}\n\n'.format(seq, timestamp, text)
+
 
 def prepareSubtitle(videoFile):
     cap = cv2.VideoCapture(videoFile)
-    frameRate = int(ceil(cap.get(cv2.CAP_PROP_FPS))) #frame rate
-    totalFrames = int(ceil(cap.get(cv2.CAP_PROP_FRAME_COUNT))) #frame ratecap.get(cv2.CV_CAP_PROP_FRAME_COUNT)
+    frameRate = int(ceil(cap.get(cv2.CAP_PROP_FPS)))  # frame rate
+    # frame ratecap.get(cv2.CV_CAP_PROP_FRAME_COUNT)
+    totalFrames = int(ceil(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
     framesToGet = int(floor(totalFrames/frameRate))
     return ["" for i in range(framesToGet)]
+
 
 def getText(frame, config=('-l eng --oem 1 --psm 3')):
     return image_to_string(frame, config=config)
 
+
 def writeToFile(subtitles, path):
-    with open(path, "w") as file: 
+    with open(path, "w") as file:
         file.writelines(subtitles)
--- test.py
+++ test.py
@@ -11,19 +11,21 @@ cap = cv2.VideoCapture(videoFile)
 frameRate = frame.getFrameRate(videoFile)
 count = 1
 
-cap.set(cv2.CAP_PROP_POS_AVI_RATIO,1)
+cap.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
 totalSeconds = cap.get(cv2.CAP_PROP_POS_MSEC)/1000 - 1
-cap.set(cv2.CAP_PROP_POS_AVI_RATIO,0)
+cap.set(cv2.CAP_PROP_POS_AVI_RATIO, 0)
 
 while(cap.isOpened()):
-    frameId = cap.get(1) #current frame number
+    frameId = cap.get(1)  # current frame number
     ret, fr, = cap.read()
 
     if (ret != True):
         break
-    
+
     sec = math.floor(cap.get(cv2.CAP_PROP_POS_MSEC)/1000)
-    print('Analysing second {}. {} seconds left'.format(sec, int(math.ceil(totalSeconds)) - sec), end='\r')
+    secLeft = int(math.ceil(totalSeconds)) - sec
+    message = 'Analysing second {}. {} seconds left'.format(sec, secLeft)
+    print(message, end='\r')
     if (frameId % math.floor(frameRate) == 0):
         subtitleArea = frame.getSubtitleArea(fr)
         if (not frame.isBlack(subtitleArea)):
@@ -31,9 +33,6 @@ while(cap.isOpened()):
             timestamp = subtitle.getTimeStamp(sec)
             subtitles[sec] = subtitle.getSubtitleGroup(count, timestamp, text)
             count += 1
-        
+
 cap.release()
 subtitle.writeToFile(subtitles, "video.srt")
-
-
-
